44e74e1db96e236234641fabc2dff0ddfb650acf
Svetlana Yarullina <svetlanaiarullina@yandex.ru>
2024-02-16 17:07:49 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,21 @@ import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+    root: './src',
+    build: {
+        outDir: resolve(__dirname, 'dist'),
+        rollupOptions: {
+            input: {
+                index: '/index.html',
+                authorization: '/pages/authorization/authorization.html',
+                registration: '/pages/registration/registration.html',
+                chat: '/pages/chat/chat.html',
+                profile: '/pages/profile/profile.html',
+                page404: '/pages/404-page/404-page.html',
+                page500: '/pages/500-page/500-page.html',
+            }
+        }
+    },
     plugins: [handlebars( {
         partialDirectory: resolve(__dirname, 'src/components'),
     })],
