1611064e3d9cfc656599231b63784f418e95aa4f
Svetlana Yarullina <svetlanaiarullina@yandex.ru>
2024-03-08 19:42:29 +0300

    fix

--- src/services/validation/validation.ts
+++ src/services/validation/validation.ts
@@ -1,35 +1,34 @@
 import {
-    messagePattern,
-    namePattern,
-    passwordPattern,
-    phonePattern,
-    loginPattern,
-    emailPattern,
-  } from './regex_patterns';
-  
-  export class Validator {
-    validateMessage(message: string) {
-      return messagePattern.test(message);
-    }
-  
-    validateLogin(login: string) {
-      return loginPattern.test(login);
-    }
-  
-    validatePassword(password: string) {
-      return passwordPattern.test(password);
-    }
-  
-    validateEmail(email: string) {
-      return emailPattern.test(email);
-    }
-  
-    validateName(name: string) {
-      return namePattern.test(name);
-    }
-  
-    validatePhone(phone: string) {
-      return phonePattern.test(phone);
-    }
+  messagePattern,
+  namePattern,
+  passwordPattern,
+  phonePattern,
+  loginPattern,
+  emailPattern,
+} from './regex_patterns';
+
+export class Validator {
+  validateMessage(message: string) {
+    return messagePattern.test(message);
   }
-  
+
+  validateLogin(login: string) {
+    return loginPattern.test(login);
+  }
+
+  validatePassword(password: string) {
+    return passwordPattern.test(password);
+  }
+
+  validateEmail(email: string) {
+    return emailPattern.test(email);
+  }
+
+  validateName(name: string) {
+    return namePattern.test(name);
+  }
+
+  validatePhone(phone: string) {
+    return phonePattern.test(phone);
+  }
+}
